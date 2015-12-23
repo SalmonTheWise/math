b6539635d832859c85a6cc7e4ceee84d1e18e25b
SalmonTheWise <raisa.chetverikova@gmail.com>
2015-12-23 06:11:31 +0300

    hw7

--- hw7_1.py
+++ hw7_1.py
@@ -1,1 +1,11 @@
 __author__ = 'salmon-the-wise'
+a = [1, 5, 11, 12]
+b = 7
+
+
+def closest_gene(gen, pos):
+    n = [abs(i-pos) for i in gen]
+    idx = n.index(min(n))
+    return gen[idx]
+
+print(closest_gene(a, b))
